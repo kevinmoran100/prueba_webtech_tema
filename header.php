af38b90d304d3f3715f1f10cb8104a12848b3f58
kevin <kevinalbertoorellana@gmail.com>
2018-09-19 22:09:35 -0600

    2

--- header.php
+++ header.php
@@ -7,6 +7,28 @@
     <?php wp_head(); ?>
   </head>
   <body>
+    <header class="header">
+      <div class="row j-center">
+        <a href="https://techweb.press/">
+          <figure class="logo">
+            <img class="logo__img" src="https://techweb.press/wp-content/themes/tech-demo/assets/img/logo.png" alt="">
+          </figure>
+        </a>
+      </div>
+
+      <nav class="social__nav"><ul class="nav j-end"><li id="menu-item-22" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22"><a href="https://www.facebook.com/techwebguatemala" class="nav-link nav__link b-red t-white t-hover-white b-hover-black"><span class="sr-text">Facebook</span></a></li>
+    <li id="menu-item-23" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-23"><a href="https://twitter.com/techwebgt" class="nav-link nav__link b-red t-white t-hover-white b-hover-black"><span class="sr-text">Twitter</span></a></li>
+    </ul></nav>
+            <nav class="navbar navbar-expand-lg b-black">
+              <button class="navbar-toggler nav__icon" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <i class="fa fa-bars t-white"></i>
+              </button>
+              <div id="collapsibleNavbar" class="collapse navbar-collapse header__nav row b-black t-white j-center"><ul id="main__menu" class="nav navbar-nav"><li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-8 current_page_item menu-item-21"><a href="https://techweb.press/" class="nav-link t-white t-hover-red">Inicio</a></li>
+    <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="https://techweb.press/catalogos-revistas-digitales/" class="nav-link t-white t-hover-red">Catálogos / Revistas digitales</a></li>
+    <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19"><a href="https://techweb.press/formularios/" class="nav-link t-white t-hover-red">Formularios</a></li>
+    </ul></div>
+        </nav>
+    </header>
     <header>
       <h1><?php bloginfo('name'); ?></h1>
     </header>
