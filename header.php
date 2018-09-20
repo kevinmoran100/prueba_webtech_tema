0f9a5e6d5649b3a18075d710dd293b752a0f46ff
kevin <kevinalbertoorellana@gmail.com>
2018-09-19 22:22:47 -0600

    e

--- header.php
+++ header.php
@@ -18,16 +18,12 @@
 
       <nav class="social__nav"><ul class="nav j-end"><li id="menu-item-22" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22"><a href="https://www.facebook.com/techwebguatemala" class="nav-link nav__link b-red t-white t-hover-white b-hover-black"><span class="sr-text">Facebook</span></a></li>
     <li id="menu-item-23" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-23"><a href="https://twitter.com/techwebgt" class="nav-link nav__link b-red t-white t-hover-white b-hover-black"><span class="sr-text">Twitter</span></a></li>
-    </ul></nav>
-            <nav class="navbar navbar-expand-lg b-black">
-              <button class="navbar-toggler nav__icon" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <i class="fa fa-bars t-white"></i>
-              </button>
-              <div id="collapsibleNavbar" class="collapse navbar-collapse header__nav row b-black t-white j-center"><ul id="main__menu" class="nav navbar-nav"><li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-8 current_page_item menu-item-21"><a href="https://techweb.press/" class="nav-link t-white t-hover-red">Inicio</a></li>
-    <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="https://techweb.press/catalogos-revistas-digitales/" class="nav-link t-white t-hover-red">Catálogos / Revistas digitales</a></li>
-    <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19"><a href="https://techweb.press/formularios/" class="nav-link t-white t-hover-red">Formularios</a></li>
-    </ul></div>
-        </nav>
+    </ul>
+    <nav>
+      <ul class="main-nav">
+        <?php wp_nav_menu( array( 'theme_location' => 'navegation' ) ); ?>
+      </ul>
+    </nav>
     </header>
     <header>
       <h1><?php bloginfo('name'); ?></h1>
